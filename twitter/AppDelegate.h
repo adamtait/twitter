153b8cfb619e0f6161676324c316ede6913b3eeb
Adam Tait <adam@adamtait.com>
2014-01-29 23:10:53 -0800

    create blank iOS app project

---
+++ twitter/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  twitter
+//
+//  Created by Adam Tait on 1/29/14.
+//  Copyright (c) 2014 Adam Tait. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ twitter/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  twitter
+//
+//  Created by Adam Tait on 1/29/14.
+//  Copyright (c) 2014 Adam Tait. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
