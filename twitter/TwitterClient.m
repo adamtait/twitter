4c92f88d30e29e40aecc5299e76b0bf8ddff5f48
Adam Tait <adam@adamtait.com>
2014-01-31 01:36:30 -0800

    swap out Tim`s dev.twitter.com app key for mine

--- twitter/TwitterClient.m
+++ twitter/TwitterClient.m
@@ -10,8 +10,8 @@
 #import "AFNetworking.h"
 
 #define TWITTER_BASE_URL [NSURL URLWithString:@"https://api.twitter.com/"]
-#define TWITTER_CONSUMER_KEY @"biYAqubJD0rK2cRatIQTZw"
-#define TWITTER_CONSUMER_SECRET @"2cygl2irBgMQVNuWJwMn6vXiyDnWtht7gSyuRnf0Fg"
+#define TWITTER_CONSUMER_KEY @"I6DFKpf9tNscxPhdLDkYsA"
+#define TWITTER_CONSUMER_SECRET @"vtr6Vm590pcWzS9tHwqOVelCoJqTnzAaKHX5d1II"
 
 static NSString * const kAccessTokenKey = @"kAccessTokenKey";
 
